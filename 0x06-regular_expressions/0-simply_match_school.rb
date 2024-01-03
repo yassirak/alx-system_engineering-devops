b841d968232bc1e9885c1239925d46532659ddc3
yassirak <yasser.karimi.safi@gmail.com>
2024-01-03 12:59:14 -0800

    vc

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # This script accepts one argument and passes it to a regular expression,
 # matching method
-# The regular expression must match the given cases
+# The regular expression must match School
 
-puts ARGV[0].scan(/hbt{2,5}n/).join
+puts ARGV[0].scan(/School/).join
