3892fe95352a81faac0074550b28c4a5cbd12151
yassirak <yasser.karimi.safi@gmail.com>
2024-02-25 04:08:20 -0800

    v

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # puppet declarative script to install flask from pip3.
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package {'flask':
+ensure => '2.1.0',
+provider => 'pip3',
 }
 package {'werkzeug':
 ensure => '2.1.1',
